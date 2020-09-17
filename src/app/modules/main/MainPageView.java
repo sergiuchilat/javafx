66bce1cac7405c2265c5193246a3e33c04a76ba3
Sergiu Chilat <chilatsergiu@gmail.com>
2020-09-18 00:12:41 +0300

    add data in modal

--- src/app/modules/main/MainPageView.java
+++ src/app/modules/main/MainPageView.java
@@ -5,18 +5,24 @@ import app.models.StudentModel;
 import app.modules.auth.LoginFormView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -63,14 +69,8 @@ public class MainPageView extends View implements Initializable {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 Stage mainStage = (Stage) backToLoginBtn.getScene().getWindow();
-                Popup createPopup = new Popup();
-                TextField nameInput = new TextField();
-                nameInput.setText("Name");
-                createPopup.getContent().add(nameInput);
-                createPopup.show(mainStage);
             }
         });
-
 
         try{
             studentId.setCellValueFactory(new PropertyValueFactory<>("Id"));
@@ -83,5 +83,18 @@ public class MainPageView extends View implements Initializable {
 
         //System.out.println(students);
 
+    }
+    @FXML
+    void onOpenDialog(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CreateDialogView.fxml"));
+        Parent parent = fxmlLoader.load();
+        CreateDialogView createDialogView = fxmlLoader.<CreateDialogView>getController();
+        createDialogView.setAppMainObservableList(students);
+
+        Scene scene = new Scene(parent, 300, 200);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
     }
 }
